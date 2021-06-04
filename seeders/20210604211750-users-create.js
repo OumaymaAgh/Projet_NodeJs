55d322c4fc49708ce9418ac00e9b523ee55e9fc0
Oumayma Agherai <agherai06@gmail.com>
2021-06-05 00:32:22 +0200

    Exercice14

---
+++ seeders/20210604211750-users-create.js
@@ -0,0 +1,26 @@
+'use strict';
+const faker = require('faker');
+ const rol= [
+  'admin', 'author', ' guest'
+];
+const users = [...Array(20)].map((user) => (
+  {
+
+    userName: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(8),
+    role: faker.random.arrayElement([
+      "admin", "author", "guest"
+    ]),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+))
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', users, {});
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
